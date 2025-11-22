c224c86b5e56eb4fc8420413aaca4484808d0c7b
rick <rickoliveiraa111@gmail.com>
2025-11-22 20:52:47 -0300

    salvar

--- projeto-final-rick/app/_layout.js
+++ projeto-final-rick/app/_layout.js
@@ -1,7 +1,14 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Tabs } from 'expo-router';
+import { useEffect } from 'react';
+import { Platform } from 'react-native';
 
 export default function TabLayout() {
+
+  useEffect(()=>{
+    Platform.OS === "web" ? document.title = "DDM 2025/2" : null 
+  }, [])
+  
   return (
     <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
       <Tabs.Screen
